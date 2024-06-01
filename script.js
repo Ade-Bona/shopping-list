5bf2cc5983387aa10c7c04f8be8e2b96c6fbbea4
unknown <adebolaomoolorun1@gmail.com>
2024-06-01 17:04:10 +0100

    Initital commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const itemForm = document.querySelector('#item-form')
+const itemInput = document.querySelector('#item-input')
+const itemList = document.querySelector('#item-list')
+
+const addItem = (e) => {
+    e.preventDefault()
+
+    const newItem = itemInput.value
+
+    // Validate Input
+    if (newItem === '') {
+        alert('Please add an item');
+        return; // This is because we dont want anything else to happen after
+    }
+
+    // Create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(newItem))
+
+    const button = createButton('remove-item btn-link text-red')
+    li.appendChild(button)
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+}
+
+const createButton = (classes) => {
+    const button = document.createElement('button')
+    button.className = classes
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon)
+    return button
+}
+
+const createIcon = (classes) => {
+    const icon = document.createElement('i')
+    icon.className = classes
+    return icon
+}
+
+// Event Listeners
+itemForm.addEventListener('submit', addItem)
